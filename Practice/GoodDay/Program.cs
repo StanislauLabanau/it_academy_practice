ac4f0a92403c4ee0ef79b4c191c37f66e2ee640e
StanislauLabanau <stanislau.labanau@gmail.com>
2020-06-08 23:12:00 +0300

    fixed, actually it's second task with the name of the first

--- Practice/GoodDay/Program.cs
+++ Practice/GoodDay/Program.cs
@@ -7,19 +7,23 @@ namespace GoodDay
         static void Main(string[] args)
         {
             int count = 0;
+
             Console.WriteLine("enter the word");
+
             string wordConsole = Console.ReadLine();
+
             for (int i = 0; i < wordConsole.Length; i++)
             {
                 if (wordConsole[i] == 'a')
                 {
-                    count = +count;
+                    count++;
                 }
-
-                count = +count;
             }
 
-                Console.WriteLine(count);
+            Console.WriteLine("word " + wordConsole + " has " + count + " letters a");
+            Console.ReadKey();
+
+
         }
     }
 }
