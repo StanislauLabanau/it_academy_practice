1ea03a02bda9976715975966bf3064cdc0dbab82
StanislauLabanau <stanislau.labanau@gmail.com>
2020-06-10 20:49:33 +0300

    task_1

---
+++ Practice/Lecture.6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lecture._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmptyArray();
+            Console.ReadKey();
+        }
+
+        static void EmptyArray()
+        {
+            int[] someArray = new int[] { };
+            Console.WriteLine("The empty array has created");
+        }
+    }
+}
