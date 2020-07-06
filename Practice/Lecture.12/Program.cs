fb97ee0d4d720a3c2b2e7bca0dc4c98c748c26a6
StanislauLabanau <stanislau.labanau@gmail.com>
2020-07-06 20:04:39 +0300

    Lecture 12

---
+++ Practice/Lecture.12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lecture._12
+{
+    public class Uniqueltem
+    {
+        static int Id { get; set; }
+
+        static Uniqueltem()
+        {
+            Id = Id + 1;
+        }
+
+        public static void SetFirstId(int id)
+        {
+            Id = id;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uniqueltem.SetFirstId(10);
+            Uniqueltem object1 = new Uniqueltem();
+            Uniqueltem object2 = new Uniqueltem();
+            Uniqueltem object3 = new Uniqueltem();
+        }
+    }
+}
