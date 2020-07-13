f9320161ac6ca990bec8068c64e90e01770ed683
StanislauLabanau <stanislau.labanau@gmail.com>
2020-07-13 20:12:21 +0300

    task 3 finished

--- Practice/Lecture.14.Task3/Program.cs
+++ Practice/Lecture.14.Task3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lecture._14.Task3
 {
@@ -6,7 +7,18 @@ namespace Lecture._14.Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Neighbor> floorNeighbours = new List<Neighbor>();
+            floorNeighbours.Add(new Neighbor("first", "1", "1111"));
+            floorNeighbours.Add(new Neighbor("second", "2", "2222"));
+            floorNeighbours.Add(new Neighbor("third", "3", "3333"));
+
+            Console.WriteLine("Enter flat number");
+            string flatNumber = Console.ReadLine();
+                       
+            foreach (Neighbor neighbor in floorNeighbours)
+            {
+                neighbor.ShowNumber(flatNumber);
+            }
         }
     }
 }
