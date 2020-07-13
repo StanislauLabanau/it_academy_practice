50cc1ff27b1dc9fb5b62d857d02b8856dadd1adb
StanislauLabanau <stanislau.labanau@gmail.com>
2020-07-13 20:01:14 +0300

    task1 and a part of task 3

---
+++ Practice/Lecture.14.Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lecture._14.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
